9510096aabdb43f5d8fce2e2401f5c7c06ad86e5
Adam Findlay <skylinerspeeder@gmail.com>
2017-05-31 15:42:47 +0100

    Built url structure

--- capritools/urls.py
+++ capritools/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf import settings
+
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', include('capritools2.urls'))
 ]
