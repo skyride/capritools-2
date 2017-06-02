b6bdd94dfcb9754674c468b0d89aaebfb3312be1
Adam Findlay <skylinerspeeder@gmail.com>
2017-06-02 19:53:46 +0100

    Added info panel

--- capritools2/views/dscan.py
+++ capritools2/views/dscan.py
@@ -18,12 +18,31 @@ def dscan_home(request):
 def dscan_view(request, key):
     scan = Dscan.objects.get(key=key)
 
-    capitals = [7, 485, 547, 659, 883, 1538]
+    supers = [659, 30]
+    capitals = [485, 547, 883, 1538] + supers
+
+    info = scan.scanObjects.filter(
+        item__group__category_id=6
+    ).exclude(
+        item__group_id__in=supers
+    ).annotate(
+        items_mass=Sum('item__mass'),
+        items_volume=Sum('item__volume')
+    ).aggregate(
+        total_mass=Sum('items_mass'),
+        total_volume=Sum('items_volume')
+    )
+
+    # Calculate bridge usage
+    info['titan_topes'] = info['total_mass'] * 1500 * 0.000000001 * 0.6 * 6
+    info['blops_topes'] = info['total_mass'] * 450 * 0.000000135 * 0.6 * 8
 
     return render_page(
         "capritools2/dscan_view.html",
         {
             'scan': scan,
+            'info': info,
+
             'ship_count': scan.scanObjects.filter(item__group__category_id=6).count(),
             'ships': Item.objects.filter(
                     group__category_id=6,
